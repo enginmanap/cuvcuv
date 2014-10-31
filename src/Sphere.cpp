3fa03e18de665d54b7d9bbb2b04541c77cf1dce0
Engin Manap <enginmanap@gmail.com>
2014-10-31 17:43:19 +0200

    add intersectionTest to Sphere

--- src/Sphere.cpp
+++ src/Sphere.cpp
@@ -19,6 +19,29 @@ Sphere::~Sphere() {
 	// TODO Auto-generated destructor stub
 }
 
-bool Sphere::intersectiontest(Ray){
+bool Sphere::intersectiontest(Ray ray){
+	//the equation is:
+	// t^2 * (P1 * P1) + 2 * t * P1 * (P0 - C) + (P0 -C)^2 -r^2 = 0
+	float p1s = Vec3f::dot(ray.getDirection(), ray.getDirection());
+	Vec3f p0MinusC = ray.getPosition() - this->position;
+	float p1timesp0mc = Vec3f::dot(ray.getDirection(), p0MinusC);
+	float t;
+	//now the formula is this:
+	//(t * t) * p1s + t * (2*p1timesp0mc) + (p0MinusC * p0MinusC) - (this->radius * this->radius) = 0
+
+	//calculate the discriminant
+	// discriminant = b^2 - 4ac
+	float dis= (2*p1timesp0mc) * (2*p1timesp0mc) - ( 4* (p1s * ((Vec3f::dot(p0MinusC, p0MinusC)) - (this->radius * this->radius))));
+	//if d > 0, 2 solutions, if d=0 2 solutions are equal
+	// if d < 0 no real solutions
+
+	if(dis > 0){
+		return true;
+	} else if (dis == 0){
+		return true;
+	} else {
+		return false;
+	}
+
 	return false;
 }
