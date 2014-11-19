7307d9f47853f987300dd80005177342c1b5e0ce
Engin Manap <enginmanap@gmail.com>
2014-11-20 01:43:26 +0200

    Fix octree generation.
    This version is around 85 times faster. yes %8500 faster.

--- src/Octree.cpp
+++ src/Octree.cpp
@@ -11,9 +11,9 @@
 
 Octree::Octree(Octree* parent, Vec3f upperEnd, Vec3f lowerEnd,
 		std::vector<Primitive*> primitives) {
-    //std::cout << "creating Octree with primitive count: " << primitives.size() << std::endl;
+	//std::cout << "creating Octree with primitive count: " << primitives.size() << std::endl;
 	static std::string level = "";
-	std::cout << level << "up: " << upperEnd << ", low: " << lowerEnd << std::endl;
+	//std::cout << level << "up: " << upperEnd << ", low: " << lowerEnd << std::endl;
 	this->parent = parent;
 	this->upperEnd = upperEnd;
 	this->lowerEnd = lowerEnd;
@@ -21,115 +21,119 @@ Octree::Octree(Octree* parent, Vec3f upperEnd, Vec3f lowerEnd,
 	this->center = ((float) 1 / 2) * temp;
 	memset(children, 0, sizeof(Octree*) * 8);
 
-	std::vector<Primitive*> contained, notContained, toCheck; //TODO this should be done by addPrimitive method.
+	std::vector<Primitive*> contained[8], notContained, partiallyContained,
+			toCheck; //TODO this should be done by addPrimitive method.
 	toCheck = primitives;
-	if (primitives.size() > 1) {
-		//we should calculate children
-		if (upperEnd.x - lowerEnd.x > 1.0f) { //if we can split more
-			//calculate the sides
-			//we need 8 up, 8 low, we have up down center^^^2, so we need 4 more
-			/**
-			 * the childs are:
-			 * x = ++++----
-			 * y = ++--++--
-			 * z = +-+-+-+-
-			 */
-			Vec3f up[8];
-			up[0] = upperEnd;
-			up[1] = upperEnd;
-			up[1].z = center.z;
-			up[2] = upperEnd;
-			up[2].y = center.y;
-			up[3] = upperEnd;
-			up[3].y = center.y;
-			up[3].z = center.z;
-			up[4] = upperEnd;
-			up[4].x = center.x;
-			up[5] = upperEnd;
-			up[5].x = center.x;
-			up[5].z = center.z;
-			up[6] = upperEnd;
-			up[6].x = center.x;
-			up[6].y = center.y;
-			up[7] = upperEnd;
-			up[7].x = center.x;
-			up[7].z = center.z;
-			up[7].y = center.y;
-			//std::cout << "up1: " << up[1]<< std::endl;
-			Vec3f down[8];
-			down[0] = center;
-			down[1] = center;
-			down[1].z = lowerEnd.z;
-			down[2] = center;
-			down[2].y = lowerEnd.y;
-			down[3] = center;
-			down[3].y = lowerEnd.y;
-			down[3].z = lowerEnd.z;
-			down[4] = center;
-			down[4].x = lowerEnd.x;
-			down[5] = center;
-			down[5].x = lowerEnd.x;
-			down[5].z = lowerEnd.z;
-			down[6] = center;
-			down[6].x = lowerEnd.x;
-			down[6].y = lowerEnd.y;
-			down[7] = center;
-			down[7].x = lowerEnd.x;
-			down[7].z = lowerEnd.z;
-			down[7].y = lowerEnd.y;
-			//std::cout << "low1: " << down[1]<< std::endl;
+	if (primitives.size() > 1 && (upperEnd.x - lowerEnd.x > 0.1f)) {
+		//we should calculate children if we can split more
 
+		//calculate the sides
+		//we need 8 up, 8 low, we have up down center^^^2, so we need 4 more
+		/**
+		 * the childs are:
+		 * x = ++++----
+		 * y = ++--++--
+		 * z = +-+-+-+-
+		 */
+		Vec3f up[8];
+		up[0] = upperEnd;
+		up[1] = upperEnd;
+		up[1].z = center.z;
+		up[2] = upperEnd;
+		up[2].y = center.y;
+		up[3] = upperEnd;
+		up[3].y = center.y;
+		up[3].z = center.z;
+		up[4] = upperEnd;
+		up[4].x = center.x;
+		up[5] = upperEnd;
+		up[5].x = center.x;
+		up[5].z = center.z;
+		up[6] = upperEnd;
+		up[6].x = center.x;
+		up[6].y = center.y;
+		up[7] = upperEnd;
+		up[7].x = center.x;
+		up[7].z = center.z;
+		up[7].y = center.y;
+		//std::cout << "up1: " << up[1]<< std::endl;
+		Vec3f down[8];
+		down[0] = center;
+		down[1] = center;
+		down[1].z = lowerEnd.z;
+		down[2] = center;
+		down[2].y = lowerEnd.y;
+		down[3] = center;
+		down[3].y = lowerEnd.y;
+		down[3].z = lowerEnd.z;
+		down[4] = center;
+		down[4].x = lowerEnd.x;
+		down[5] = center;
+		down[5].x = lowerEnd.x;
+		down[5].z = lowerEnd.z;
+		down[6] = center;
+		down[6].x = lowerEnd.x;
+		down[6].y = lowerEnd.y;
+		down[7] = center;
+		down[7].x = lowerEnd.x;
+		down[7].z = lowerEnd.z;
+		down[7].y = lowerEnd.y;
+		//std::cout << "low1: " << down[1]<< std::endl;
+
+		unsigned char notContainedCounter = 0;
+		for (std::vector<Primitive*>::iterator primIter = primitives.begin();
+				primIter != primitives.end(); ++primIter) {
+			notContainedCounter = 0;
 			for (unsigned int subtree = 0; subtree < 8; ++subtree) {
-				for (unsigned int primitive = 0; primitive < toCheck.size();
-						++primitive) {
-					switch (toCheck[primitive]->isInBoundingBox(up[subtree],
-							down[subtree])) {
-					case 2:
-						//std::cout<< "putting to subtree node " << subtree << std::endl;
-						contained.push_back(toCheck[primitive]);
-						break;
-					case 1:
-						//std::cout<< "partially contained " << subtree << std::endl;
-						contained.push_back(toCheck[primitive]);
-						notContained.push_back(toCheck[primitive]);
-						break;
-					case 0:
-						//std::cout<< "no contact with " << subtree << std::endl;
-						notContained.push_back(toCheck[primitive]);
-						break;
-					default:
-						break;
-					}
+				switch ((*primIter)->isInBoundingBox(up[subtree], down[subtree])) {
+				case 2:
+					//std::cout<< "putting to subtree node " << subtree << std::endl;
+					contained[subtree].push_back(*primIter);
+					break;
+				case 1:
+					//std::cout<< "partially contained " << subtree << std::endl;
+					contained[subtree].push_back(*primIter);
+					break;
+				case 0:
+					//std::cout<< "no contact with " << subtree << std::endl;
+					++notContainedCounter;
+					break;
+				default:
+					break;
 				}
-				std::string oldLevel = level;
-				level = level + "  ";
-				children[subtree] = new Octree(this, up[subtree], down[subtree],
-						contained);
-				level = oldLevel;
-				contained.clear();
-				toCheck.clear();
-				toCheck = notContained;
-				notContained.clear();
+			}
+			if(notContainedCounter == 8){
+				notContained.push_back(*primIter);
 			}
 		}
+		std::string oldLevel = level;
+		level = level + "  ";
+		for (unsigned int subtree = 0; subtree < 8; ++subtree) {
+			children[subtree] = new Octree(this, up[subtree], down[subtree],
+					contained[subtree]);
+			level = oldLevel;
+
+		}
+		this->primitives = std::set<Primitive*>(notContained.begin(),
+				notContained.end());
+	} else { //no children
+		this->primitives = std::set<Primitive*>(primitives.begin(),
+				primitives.end());
 	}
 
 	//std::copy( fooVec.begin(), fooVec.end(), std::inserter( fooSet, fooSet.end() ) );
 
 	//std::set<int> fooSet( fooVec.begin(), fooVec.end() );
-	this->primitives = std::set<Primitive*>(toCheck.begin(), toCheck.end());
+
 	//this->primitives = toCheck; //this leaf only has the elements that its children does not
-    std::string primitiveIds;
-    std::stringstream sstm;
-    for(std::set<Primitive*>::iterator it=this->primitives.begin(); it !=this->primitives.end();++it){
-        sstm << (*it)->id << " ";
-    }
-    primitiveIds = sstm.str();
-	std::cout << level << "resulted primitives (" << this->primitives.size() << ")" << primitiveIds
-			<< std::endl;
-	//std::cout << level << "up: " << upperEnd << ", low: " << lowerEnd
-	//		<< ", primitives: " << toCheck.size() << ", center: " << center
-	//		<< std::endl;
+//    std::string primitiveIds;
+//    std::stringstream sstm;
+//    for(std::set<Primitive*>::iterator it=this->primitives.begin(); it !=this->primitives.end();++it){
+//        sstm << (*it)->id << " ";
+//    }
+//    primitiveIds = sstm.str();
+//	std::cout << level << "resulted primitives (" << this->primitives.size() << "): " << primitiveIds
+//			<< std::endl;
 }
 
 Octree::~Octree() {
@@ -151,27 +155,21 @@ bool Octree::isRayIntersects(const Ray& ray) const {
 
 	//std::cout << "inverse 2 " << directionInverse << std::endl;
 	//std::cout << "ray " << ray.getPosition() << ", " << ray.getDirection()<< std::endl;
-    Vec4f rayPos = ray.getPosition();
-	float lowerXIntersection = (lowerEnd.x - rayPos.x)
-			* directionInverse.x;
-	float upperXIntersection = (upperEnd.x - rayPos.x)
-			* directionInverse.x;
+	Vec4f rayPos = ray.getPosition();
+	float lowerXIntersection = (lowerEnd.x - rayPos.x) * directionInverse.x;
+	float upperXIntersection = (upperEnd.x - rayPos.x) * directionInverse.x;
 
 	float tmin = std::min(lowerXIntersection, upperXIntersection);
 	float tmax = std::max(lowerXIntersection, upperXIntersection);
 
-	float lowerYIntersection = (lowerEnd.y - rayPos.y)
-			* directionInverse.y;
-	float upperYIntersection = (upperEnd.y - rayPos.y)
-			* directionInverse.y;
+	float lowerYIntersection = (lowerEnd.y - rayPos.y) * directionInverse.y;
+	float upperYIntersection = (upperEnd.y - rayPos.y) * directionInverse.y;
 
 	tmin = std::max(tmin, std::min(lowerYIntersection, upperYIntersection));
 	tmax = std::min(tmax, std::max(lowerYIntersection, upperYIntersection));
 
-	float loverZIntersection = (lowerEnd.z - rayPos.z)
-			* directionInverse.z;
-	float upperZIntersection = (upperEnd.z - rayPos.z)
-			* directionInverse.z;
+	float loverZIntersection = (lowerEnd.z - rayPos.z) * directionInverse.z;
+	float upperZIntersection = (upperEnd.z - rayPos.z) * directionInverse.z;
 
 	tmin = std::max(tmin, std::min(loverZIntersection, upperZIntersection));
 	tmax = std::min(tmax, std::max(loverZIntersection, upperZIntersection));
--- src/RayTracer.cpp
+++ src/RayTracer.cpp
@@ -15,10 +15,10 @@ RayTracer::~RayTracer() {
 
 }
 
-bool RayTracer::traceToLight(const Ray ray,
-		const Octree& octree, const Light light) const {
+bool RayTracer::traceToLight(const Ray ray, const Octree& octree,
+		const Light light) const {
 	Vec4f route = light.getPosition() - ray.getPosition();
-	float distance =  route.length();
+	float distance = route.length();
 	//std::cout << "distance to light " << distance << std::endl;
 	float intersectionDistance;
 	Primitive* intersectingPrimitive = NULL;
@@ -45,30 +45,36 @@ bool RayTracer::traceToLight(const Ray ray,
 
 }
 
-Vec3f RayTracer::trace(const Ray ray,
-		const Octree& octree, const std::vector<Light> &lights, const unsigned int depth) const {
+Vec3f RayTracer::trace(const Ray ray, const Octree& octree,
+		const std::vector<Light> &lights, const unsigned int depth) const {
+	static unsigned int totalTests = 0, mostTests = 0;
 	float distance = std::numeric_limits<float>::max(); // this is the maximum value float can have, min() returns min positive value.
 	float intersectionDistance;
 	Primitive* intersectingPrimitive = NULL;
 
 	std::set<Primitive*> primitives = octree.getIntersectingPrimitives(ray);
-	if(primitives.size() > 0){
-		//std::cout << "intersectionTest for "<< primitives.size() << std::endl;
-	for (std::set<Primitive*>::const_iterator it = primitives.begin();
-			it != primitives.end(); it++) {
-		if ((*it)->intersectiontest(ray, intersectionDistance)) {
-			//found intersection
-			if (distance > intersectionDistance) {
-				distance = intersectionDistance;
-				intersectingPrimitive = *it;
-			}
+	if (primitives.size() > 0) {
+		totalTests += primitives.size();
+		if (primitives.size() > mostTests) {
+			std::cout << "maximum tests " << primitives.size() << std::endl;
+			mostTests = primitives.size();
+		}
+		for (std::set<Primitive*>::const_iterator it = primitives.begin();
+				it != primitives.end(); it++) {
+			if ((*it)->intersectiontest(ray, intersectionDistance)) {
+				//found intersection
+				if (distance > intersectionDistance) {
+					distance = intersectionDistance;
+					intersectingPrimitive = *it;
+				}
 
+			}
 		}
 	}
-	}
-
+	std::cout << "total tests " << totalTests<< std::endl;
 	if (intersectingPrimitive != NULL) {
-		return intersectingPrimitive->getColorForRay(ray, distance, octree, lights, depth-1);
+		return intersectingPrimitive->getColorForRay(ray, distance, octree,
+				lights, depth - 1);
 	} else {
 		return Vec3f(0.0f, 0.0f, 0.0f);
 	}
