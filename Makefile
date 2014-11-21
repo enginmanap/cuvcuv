793961cf5881156c7fff97533a0b52f269d063fd
Engin Manap <enginmanap@gmail.com>
2014-11-21 16:35:48 +0200

    implement octree root calculation

--- Makefile
+++ Makefile
@@ -10,8 +10,8 @@ SDL_LIB = `sdl2-config --libs` -lSDL2_image -lSDL2_ttf
 SDL_INCLUDE = `sdl2-config --cflags`
 # You may need to change -std=c++11 to -std=c++0x if your compiler is a bit older
 #CXXFLAGS = -Wall -c -ggdb -std=c++11 $(SDL_INCLUDE) -DUSE_FREEIMAGE_PNG
-CXXFLAGS = -fopenmp -Wall -O3 -c $(SDL_INCLUDE) 
-LDFLAGS = $(SDL_LIB) -L./lib/ -lFreeImage -lgomp
+CXXFLAGS = -fopenmp -Wall -O3 -c
+LDFLAGS = -L./lib/ -lgomp
 EXE = rayTracer
 BINARY_PATH=bin/
 SOURCE_PATH=src/
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 
 #include <iostream>
-#include <SDL2/SDL.h>
 #include <string>
 #include "FileReader.h"
 #ifdef USE_FREEIMAGE_PNG
@@ -32,24 +31,18 @@ void saveToFile(unsigned char pixels[], int height, int width, std::string filen
 
 
 int main(int argc, char *argv[]) {
-	bool isTimesPrinted = false;
-	bool quit = false;
 
-	SDL_Event event;
 
 	int height = HEIGHT, width = WIDTH;
 
 	FileReader* reader;
 	Scene* scene = NULL;
 	//read the file
-	Uint32 start,readFile,buildOctree,renderAll;
-	start = SDL_GetTicks();
 	try {
 		reader = new FileReader("scene1.test");
 
 		scene = reader->readFile();
 		scene->getSamplingSize(height, width);
-		readFile = SDL_GetTicks() - start;
 		std::cout << "sample size is " << height << ", " << width << std::endl;
 	} catch (int i) {
 		std::cout << "file read failed" << "exiting" << std::endl;
@@ -58,60 +51,12 @@ int main(int argc, char *argv[]) {
 
 	unsigned char* pixels = scene->getPixels(height, width);
 
-	SDL_Init(SDL_INIT_VIDEO);
-
-	SDL_Window * window = SDL_CreateWindow("Ray Tracer",
-	SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width,height, 0);
-
-	SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
-#if SDL_BYTEORDER == SDL_BIG_ENDIAN
-	SDL_Texture * texture = SDL_CreateTexture(renderer,
-			SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, width,height);
-#else
-	SDL_Texture * texture = SDL_CreateTexture(renderer,
-			SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING, width,height);
-#endif
 
 	scene->buildOctree();
-	 buildOctree = SDL_GetTicks() - readFile;
 
-	while (!quit) {
-		if (!isTimesPrinted && scene->renderScene()){
-			renderAll = SDL_GetTicks() - buildOctree;
-			std::cout << "start: " << start/1000 << std::endl;
-			std::cout << "file read: " << readFile/1000<< std::endl;
-			std::cout << "building octree: " << buildOctree/1000 << std::endl;
-			std::cout << "render: " << renderAll/1000 << std::endl;
-			isTimesPrinted = true;
-		}
-		SDL_UpdateTexture(texture, NULL, pixels, width * 4 ); // for 4 channels
-		SDL_PollEvent(&event);
-
-		switch (event.type) {
-		case SDL_QUIT:
-
-			quit = true;
-			break;
-		}
-
-		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, texture, NULL, NULL);
-		SDL_RenderPresent(renderer);
-	}
-	if (!isTimesPrinted){
-		renderAll = SDL_GetTicks() - buildOctree;
-		std::cout << "start: " << start/1000 << std::endl;
-		std::cout << "file read: " << readFile/1000<< std::endl;
-		std::cout << "building octree: " << buildOctree/1000 << std::endl;
-		std::cout << "render: " << renderAll/1000 << std::endl;
-		isTimesPrinted = true;
-	}
+	scene->renderScene();
 	saveToFile(pixels, height, width, scene->getSaveFilename());
 	delete reader;
 	//delete scene; not needed since it is managed by reader;
-	SDL_DestroyTexture(texture);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 	return 0;
 }
