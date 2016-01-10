9fa035d7f9001476ce9936b94fcad0357db0c97c
Engin Manap <enginmanap@gmail.com>
2016-01-10 10:30:12 +0200

    if DOF rate is 1, disable randomization.

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -99,41 +99,50 @@ bool Camera::getRays(unsigned int& x, unsigned int& y, unsigned int rayCount, un
 
 		rays.clear();
 		//std::vector<Ray> temp = Ray::generateDeriveredRays(Vec4f(focalPoint,1.0),direction,up, 1.0, rayCount,xChangeFactor*0.5,yChangeFactor*0.5);
-		std::vector<Ray> focalRays;
-
-		//change ray origin based on direction u/w
-		Vec3f u,v, w, offseV;
-		Vec3f tempDirection, tempOrigin;
-		w = -1 * direction.normalize();
-		u = Vec3fNS::normalize(Vec3fNS::cross(up, w));
-		v = Vec3fNS::cross(w, u);
-
-		//FIXME We should read this value as part of camera setup
-		double aperture = 0.25;
-		double uChange,vChange;
-
-		for(unsigned int i=0; i<DOFRate;i++){
-			uChange = (rand() / double(RAND_MAX + 1) - 0.5) * aperture;
-			vChange = (rand() / double(RAND_MAX + 1) - 0.5) * aperture;
-
-			//now calculate direction from new origin to the focal point
-			tempOrigin = this->position + uChange * u + vChange * v;
-			tempDirection = (focalPoint - tempOrigin).normalize();
-
-			focalRays.push_back(Ray(tempOrigin,tempDirection,1.0,100));
-			//std::cout << temp[temp.size()-1] << ", target " << temp[temp.size()-1].getPosition() + FocalDistance.length() * temp[temp.size()-1].getDirection() << std::endl;
-
-		}
-
-
-		//std::cout << std::endl;
 		
-		for(std::vector<Ray>::iterator rayIt = focalRays.begin();
-		rayIt != focalRays.end(); ++ rayIt){
-			std::vector<Ray> temp = Ray::generateDeriveredRays(rayIt->getPosition(),rayIt->getDirection(),up, 1.0, rayCount,xChangeFactor*0.5,yChangeFactor*0.5);
+		
+		if(DOFRate == 1) {
+			std::vector<Ray> temp = Ray::generateDeriveredRays(Vec4f(position, 1.0),direction,up, 1.0, rayCount,xChangeFactor*0.5,yChangeFactor*0.5);
 			rays.insert(rays.end(), temp.begin(),temp.end());
+
+		} else {
+		//this means we need to generate rays for depth of field too.
+			std::vector<Ray> focalRays;
+
+			//change ray origin based on direction u/w
+			Vec3f u,v, w, offseV;
+			Vec3f tempDirection, tempOrigin;
+			w = -1 * direction.normalize();
+			u = Vec3fNS::normalize(Vec3fNS::cross(up, w));
+			v = Vec3fNS::cross(w, u);
+
+			//FIXME We should read this value as part of camera setup
+			double aperture = 0.25;
+			double uChange,vChange;
+
+			
+			for(unsigned int i=0; i<DOFRate;i++){
+				uChange = (rand() / double(RAND_MAX + 1) - 0.5) * aperture;
+				vChange = (rand() / double(RAND_MAX + 1) - 0.5) * aperture;
+
+				//now calculate direction from new origin to the focal point
+				tempOrigin = this->position + uChange * u + vChange * v;
+				tempDirection = (focalPoint - tempOrigin).normalize();
+
+				focalRays.push_back(Ray(tempOrigin,tempDirection,1.0,100));
+				//std::cout << temp[temp.size()-1] << ", target " << temp[temp.size()-1].getPosition() + FocalDistance.length() * temp[temp.size()-1].getDirection() << std::endl;
+
+			}
+
+
+			//std::cout << std::endl;
+			
+			for(std::vector<Ray>::iterator rayIt = focalRays.begin();
+			rayIt != focalRays.end(); ++ rayIt){
+				std::vector<Ray> temp = Ray::generateDeriveredRays(rayIt->getPosition(),rayIt->getDirection(),up, 1.0, rayCount,xChangeFactor*0.5,yChangeFactor*0.5);
+				rays.insert(rays.end(), temp.begin(),temp.end());
+			}
 		}
-        
 
 		return true;
 	} else {
